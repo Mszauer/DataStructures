1f3c8789c29a50bdabf85b6c59ba006ba1095448
Martin <mszauer@mail.usf.edu>
2015-07-28 14:26:43 -0700

    added dowhile loop

--- Simon/Program.cs
+++ Simon/Program.cs
@@ -3,17 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataStructures;
+using Datastructures;
 
-namespace DataStructures {
+namespace Datastructures {
     class Program {
         static void Main(string[] args) {
             Simon game = new Simon();
-            game.Directions("Watch", System.Convert.ToString(3));
-            game.Directions("Watch", System.Convert.ToString(2));
-            game.Directions("Watch", System.Convert.ToString(1));
-            game.Directions("Watch", "Go!");
-            game.WriteKeys();
+            do {
+                game.CreateColors();
+                game.Directions("Watch", System.Convert.ToString(3));
+                game.Directions("Watch", System.Convert.ToString(2));
+                game.Directions("Watch", System.Convert.ToString(1));
+                game.Directions("Watch", "Go!");
+                game.WriteKeys();
+                game.HighLightKeys();
+                game.Directions("Repeat", System.Convert.ToString(3));
+                game.Directions("Repeat", System.Convert.ToString(2));
+                game.Directions("Repeat", System.Convert.ToString(1));
+                game.Directions("Repeat", "Go!");
+            } while (game.CorrectKeys());
         }
         
     }
