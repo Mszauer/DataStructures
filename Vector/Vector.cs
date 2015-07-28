beb26043ab9d54c60ee53f4a90dc2f9c71615d90
Martin <mszauer@mail.usf.edu>
2015-07-27 17:09:32 -0700

    passed all unit tests

--- Vector/Vector.cs
+++ Vector/Vector.cs
@@ -65,26 +65,28 @@ namespace Datastructures {
             // If the requested capacity is greater than the current capacity, increase
             // the current capacity to the requested capacity by ceateing a new array
             // dont forget to copy the old data!
-            /*
+            
             T[] _data = new T[requestedCapacity];
             if (requestedCapacity > capacity) {
                 for (int i = 0; i < data.Length; i++) {
                     _data[i] = data[i];
+                    capacity = requestedCapacity;
+
                 }
             }
-            capacity = requestedCapacity;
-             */
+
+            
         }
 
         public void Clear() {
             // Essentially a reset function, hold no data, with 0 size and capacity
-            /*
+            
             for (int i = 0; i < data.Length; i++) {
                 data[i] = default(T);
             }
             size = 0;
             capacity = 0;
-             */
+             
         }
     }
 }
