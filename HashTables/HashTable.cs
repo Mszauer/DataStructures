ab83ca4baf54bca9089ae4c458d05c41d8975e67
Martin <mszauer@mail.usf.edu>
2015-08-03 08:42:52 -0700

    no idea what I am doing

--- HashTables/HashTable.cs
+++ HashTables/HashTable.cs
@@ -20,10 +20,22 @@ namespace DataStructures {
         HashFunc hash = null;
         public void HashTable(int numBuckets, HashFunc hash) {
             this.hash = hash;
-            buckets = SLinkedList<KVP>[numBuckets](); // why is this an error?
+            //initialize the array
+            SLinkedList<KVP>[] buckets = new SLinkedList<KVP>[numBuckets];
+            //put linked list into each of the array spots
+            for (int i = 0 ; i < buckets.Length ; i++){
+                buckets[i] = new SLinkedList<KVP>();
+            }
+            //fill in the list with data
+            for (int i = 0 ; i < buckets.Length ; i++){
+                for (int j = 0 ; j < numBuckets ; j++){
+                    buckets[i].AddHead(null);
+                }
+            }
         }
         public SLinkedList<K> Keys {
             get {
+
                 return new SLinkedList<K>();
             }
         }
@@ -33,16 +45,49 @@ namespace DataStructures {
             }
         }
         public void Add(K key, V value) {
+            KVP val = new KVP(key, value);
+            //how do i add to what, what coditions no clue
+            if (buckets[hash(key)] != null) {
+                buckets[hash(key)].AddHead(val);
+            }
+            else {
 
+            }
+            size++;
         }
         public void Remove(K key) {
-
+            //loop until key is found, remove key
+            for (int i = buckets.Length -1; i >= 0 ; i--) {
+                if (buckets[i] == key) { //what? what do I even compare
+                    buckets[i] = buckets[i - 1];
+                }
+            }
+            size--;
         }
         public int Hash(K key) {
-            return 0;
+            return hash(key) ;
         }
         public V this[K key] {
-
+            get {
+                for (int i = 0; i < buckets.Length; i++) {
+                    if (buckets[i] == hash(key)) { //still have no clue how to compare
+                        return buckets[i].Value; //what
+                    }
+                    else {
+                        throw new SystemException();
+                    }
+                }
+            }
+            set {
+                for (int i = 0; i < buckets.Length; i++) {
+                    if (buckets[i] == hash(key)) {
+                        buckets[i].Value = value; //yep no clue here either
+                    }
+                    else {
+                        throw new SystemException();
+                    }
+                }
+            }
         }
     }
 }
