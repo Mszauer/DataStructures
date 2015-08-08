c63d62909db6bd5994e586a4d1ed5ff348aa189f
Martin <mszauer@mail.usf.edu>
2015-08-08 09:29:58 -0700

    Frequency table passed unit tests

---
+++ Huffman/Huffman.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures {
+    partial class Huffman {
+        public static int HashChar(char c) {
+            return (int)c;
+        }
+
+        public static HashTable<char, int> MakeFrequencyTable(string input) {
+            HashTable<char, int> result = new HashTable<char, int>(char.MaxValue, HashChar);
+            //loop through the input string
+            foreach (char c in input) {
+                //check if key exists (get the keys of the result hash table)
+                SLinkedList<char> chars = result.Keys;
+                int index = chars.IndexOf(c);
+                //check if key is in hashtable / SLL
+                if (index < 0){ //checks if it contains the char
+                    //add current char with count of 1
+                    result.Add(c,1);
+                }
+                else{
+                    //increment count of the character
+                    result[c] += 1;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Huffman/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using DataStructures;
+
+namespace DataStructures {
+    class Program {
+        static void Error(string error) {
+            ConsoleColor old = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(error);
+            Console.ForegroundColor = old;
+
+            Console.WriteLine("Press return to continue");
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args) {
+            Console.WriteLine("sizeof(int):  " + sizeof(int) + " bytes, " + (sizeof(int) * 8) + " bits");
+            Console.WriteLine("sizeof(char): " + sizeof(char) + " bytes, " + (sizeof(char) * 8) + " bits");
+            Console.WriteLine("sizeof(byte): " + sizeof(byte) + " bytes, " + (sizeof(byte) * 8) + " bits\n");
+
+            string testString = "sailing down the mississippi river";
+
+            HashTable<char, int> frequencyTable = Huffman.MakeFrequencyTable(testString);
+            Console.WriteLine("Frequency Table: ");
+            SLinkedList<char> frequencyKeys = frequencyTable.Keys;
+            for (int i = 0; i < frequencyKeys.Size; ++i) {
+                Console.Write(frequencyKeys[i] + ":" + frequencyTable[frequencyKeys[i]] + " ");
+            }
+            Console.Write("\n");
+
+            HashTable<char, int> expectedFrequency = new HashTable<char, int>(char.MaxValue, (char c) => {
+                return (int)c;
+            });
+            expectedFrequency.Add('w', 1);
+            expectedFrequency.Add('v', 1);
+            expectedFrequency.Add('t', 1);
+            expectedFrequency.Add('s', 5);
+            expectedFrequency.Add('r', 2);
+            expectedFrequency.Add('p', 2);
+            expectedFrequency.Add('o', 1);
+            expectedFrequency.Add('n', 2);
+            expectedFrequency.Add('m', 1);
+            expectedFrequency.Add('l', 1);
+            expectedFrequency.Add('i', 7);
+            expectedFrequency.Add('h', 1);
+            expectedFrequency.Add('g', 1);
+            expectedFrequency.Add('e', 2);
+            expectedFrequency.Add('d', 1);
+            expectedFrequency.Add('a', 1);
+            expectedFrequency.Add(' ', 4);
+
+            SLinkedList<char> expectedKeys = expectedFrequency.Keys;
+            for (int i = 0; i < expectedKeys.Size; ++i) {
+                if (frequencyKeys.IndexOf(expectedKeys[i]) < 0) {
+                    Error("Key " + expectedKeys[i] + " was not found in frequency table");
+                    return;
+                }
+            }
+            for (int i = 0; i < frequencyKeys.Size; ++i) {
+                if (expectedKeys.IndexOf(frequencyKeys[i]) < 0) {
+                    Error("Unexpected key " + frequencyKeys[i] + " in frequency table");
+                    return;
+                }
+            }
+
+            for (int i = 0; i < frequencyKeys.Size; ++i) {
+                if (frequencyTable[frequencyKeys[i]] != expectedFrequency[expectedKeys[i]]) {
+                    Error("Expecting \"" + frequencyKeys[i] + "\" to equal " + expectedFrequency[expectedKeys[i]] + ", not " + frequencyTable[frequencyKeys[i]]);
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
