856d11b352ce7671bd358eeca7e61e4fa8e0edbd
Martin <mszauer@mail.usf.edu>
2015-08-09 10:23:24 -0700

    huffmancompress done

---
+++ Huffman/HuffmanCompress.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures {
+    partial class Huffman {
+        private static byte[] IntToBytes(int num) {
+            return BitConverter.GetBytes(num);
+        }
+        private static byte[] CharToBytes(char c) {
+            return BitConverter.GetBytes(c);
+        }
+        public static byte[] Compress(string toCompress) {
+            //then at the end of the function we're going to convert it to an array, and return it.
+            SLinkedList<byte> result = new SLinkedList<byte>();
+            
+            //make an encoding table out of the huffman tree, out of a frequency table
+            HashTable<char, int> FreqTable = MakeFrequencyTable(toCompress); //copy to memory because we access it multiple times
+            HashTable<char,BitStream> encodingTable = MakeEncodingTable(MakeHuffmanTree(FreqTable),new BitStream()); // add to memory because accessed twice
+            //convert the size of the frequency table to bytes.
+            byte[] freqToByte = IntToBytes(FreqTable.Size);
+            //and all four bytes to the resuld linked list
+            for (int i = 0; i < freqToByte.Length; i++) {
+                result.AddHead(freqToByte[i]);
+            }
+
+            //next loop through the frequency table
+            SLinkedList<char> list = FreqTable.Keys;
+            for (int i = 0; i < list.Size; i++) {
+                //convert current key to two bytes
+                byte[] key = CharToBytes(list[i]);
+                //add both bytes to the linked list
+                result.AddHead(key[0]);
+                result.AddHead(key[1]); // O(1) vs O(n) even though it is trivial enough to be small
+                //convert the current value to foure bytes,
+                byte[] value = IntToBytes(FreqTable[list[i]]);
+                //add alll four bytes to the linked list
+                for (int j = 0; j < value.Length; j++) {
+                    result.AddHead(value[j]);
+                }
+            }//end loop
+                
+            //make new bitstream (datastream) to hold bits
+            BitStream datastream = new BitStream();
+
+            //loop through the string
+            for (int i = 0; i < toCompress.Length; i++) {
+                //append the bitstream of the encoding table to the newely created bitstream
+                datastream.Append(encodingTable[toCompress[i]]);
+            }//end loop
+
+            //get the bit streams bit count as an integer
+            int streamBitCount = datastream.BitCount;
+            //convert this integer to bytes and add it to the result list
+            byte[] streambitByte = IntToBytes(streamBitCount);
+            for (int i = 0; i < streambitByte.Length; i++) {
+                result.AddHead(streambitByte[i]);
+            }
+            //finally convert the result list to a byte array.
+            byte[] byteResult = new byte[result.Size];
+            for (int i = 0; i < byteResult.Length; i++) {
+                byteResult[i] = result[i];
+            }
+            //return the array
+            return byteResult;
+        }
+    }
+}
