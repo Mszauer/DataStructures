9732dc816a8e9dcbbcb8d1698ff1834fa3a14915
Martin <mszauer@mail.usf.edu>
2015-08-08 19:09:49 -0700

    fixed letters but still failed unit test

--- Huffman/HuffmanEncoding.cs
+++ Huffman/HuffmanEncoding.cs
@@ -10,48 +10,52 @@ namespace DataStructures {
             HashTable<char,BitStream> result = new HashTable<char,BitStream>(char.MaxValue,HashChar);
 
             //if the current node is not null and it is a leaf, we want to add it to result
-            if (node != null && node.Left == null && node.Right == null){
+            if (node != null && node.Left == null && node.Right == null) {
                 //we only store leaf nodes in the result array
                 //make a new bit stream
                 BitStream stream = new BitStream();
                 //append progress to it
                 stream.Append(progress);
                 //add node.data and the new stream to result
-                result.Add(node.Data,stream);
+                result.Add(node.Data, stream);
             }
-            
+
             //else if the node is not null, we are going to want to process it's childern
-            else if (node != null && node.Left != null){
-                //if left subtree is not null
+            else {
+                if (node != null && node.Left != null) {
+                    //if left subtree is not null
                     //make new bit stream
-                BitStream stream = new BitStream();
+                    BitStream stream = new BitStream();
                     //append progress
-                stream.Append(progress);
+                    stream.Append(progress);
                     //append 0 (because we are going left)
-                stream.Append(false);
+                    stream.Append(false);
                     //recursivley call this function on node.Left with the new stream we just created
                     //store the result of the previous call in a local hashtable
-                HashTable<char, BitStream> local = MakeEncodingTable(node.Left, stream);
+                    HashTable<char, BitStream> local = MakeEncodingTable(node.Left, stream);
                     //loop through the just created hash table(the return of the recursive function)
-                SLinkedList<char> list = local.Keys;
-                for (int i = 0; i < list.Size; i++) {
-                    //add each key value pair to this function's result hash table
-                    result.Add(list[i], stream); //what bitstream?
-                    //we do this because the results of each recursion bubble back up to the top.
-                    //by the time the root returns, all of the leaf nodes will be in the final hash table
-                }//end loop
-            }//end if
-            else{
-                //repeat above for the right subtree
-                BitStream stream = new BitStream();
-                stream.Append(progress);
-                stream.Append(true);
-                HashTable<char, BitStream> local = MakeEncodingTable(node.Right, stream);
-                SLinkedList<char> list = local.Keys;
-                for (int i = 0; i < list.Size; i++) {
-                    result.Add(list[i], stream);
-                }
-            }//end else
+                    SLinkedList<char> list = local.Keys;
+                    for (int i = 0; i < list.Size; i++) {
+                        //add each key value pair to this function's result hash table
+                        result.Add(list[i], stream); //what bitstream?
+                        Console.WriteLine("added: " + System.Convert.ToString(list[i]) + " " + stream);
+                        //we do this because the results of each recursion bubble back up to the top.
+                        //by the time the root returns, all of the leaf nodes will be in the final hash table
+                    }//end loop
+                }//end if
+                if (node != null && node.Right != null) {
+                    //repeat above for the right subtree
+                    BitStream stream = new BitStream();
+                    stream.Append(progress);
+                    stream.Append(true);
+                    HashTable<char, BitStream> local = MakeEncodingTable(node.Right, stream);
+                    SLinkedList<char> list = local.Keys;
+                    for (int i = 0; i < list.Size; i++) {
+                        result.Add(list[i], stream);
+                        Console.WriteLine("added: " + list[i] + " " + stream);
+                    }
+                }//end else
+            }
             return result;
         }
     }
