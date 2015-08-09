8f8db72a2a46b48bf51e35288ce9535f2f57ff04
Martin <mszauer@mail.usf.edu>
2015-08-09 11:11:21 -0700

    writting huffman testing program

--- Huffman/Program.cs
+++ Huffman/Program.cs
@@ -1,300 +1,90 @@
 ﻿using System;
 using DataStructures;
+using System.IO;
 
 namespace DataStructures {
     class Program {
-        static void Error(string error) {
-            ConsoleColor old = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(error);
-            Console.ForegroundColor = old;
-
-            Console.WriteLine("Press return to continue");
-            Console.ReadLine();
-        }
 
         static void Main(string[] args) {
-            Console.WriteLine("sizeof(int):  " + sizeof(int) + " bytes, " + (sizeof(int) * 8) + " bits");
-            Console.WriteLine("sizeof(char): " + sizeof(char) + " bytes, " + (sizeof(char) * 8) + " bits");
-            Console.WriteLine("sizeof(byte): " + sizeof(byte) + " bytes, " + (sizeof(byte) * 8) + " bits\n");
+            HashTable<string, int> data = new HashTable<string, int>(100, CustomHash);
+            Console.WriteLine("Enter a command, or help for help");
 
-            string testString = "sailing down the mississippi river";
-
-            HashTable<char, int> frequencyTable = Huffman.MakeFrequencyTable(testString);
-            Console.WriteLine("Frequency Table: ");
-            SLinkedList<char> frequencyKeys = frequencyTable.Keys;
-            for (int i = 0; i < frequencyKeys.Size; ++i) {
-                Console.Write(frequencyKeys[i] + ":" + frequencyTable[frequencyKeys[i]] + " ");
-            }
-            Console.Write("\n");
-
-            HashTable<char, int> expectedFrequency = new HashTable<char, int>(char.MaxValue, (char c) => {
-                return (int)c;
-            });
-            expectedFrequency.Add('w', 1);
-            expectedFrequency.Add('v', 1);
-            expectedFrequency.Add('t', 1);
-            expectedFrequency.Add('s', 5);
-            expectedFrequency.Add('r', 2);
-            expectedFrequency.Add('p', 2);
-            expectedFrequency.Add('o', 1);
-            expectedFrequency.Add('n', 2);
-            expectedFrequency.Add('m', 1);
-            expectedFrequency.Add('l', 1);
-            expectedFrequency.Add('i', 7);
-            expectedFrequency.Add('h', 1);
-            expectedFrequency.Add('g', 1);
-            expectedFrequency.Add('e', 2);
-            expectedFrequency.Add('d', 1);
-            expectedFrequency.Add('a', 1);
-            expectedFrequency.Add(' ', 4);
-
-            SLinkedList<char> expectedKeys = expectedFrequency.Keys;
-            for (int i = 0; i < expectedKeys.Size; ++i) {
-                if (frequencyKeys.IndexOf(expectedKeys[i]) < 0) {
-                    Error("Key " + expectedKeys[i] + " was not found in frequency table");
-                    return;
+            do {
+                string command = Console.ReadLine().ToLower();
+                string[] words = command.Split(' ');
+                if (command == "help") {
+                    Console.WriteLine("Available commands:");
+                    Console.WriteLine("\t compress < path >");
+                    Console.WriteLine("\t inflate < path >");
+                    Console.WriteLine("\t clear");
+                    Console.WriteLine("\t list");
+                    Console.WriteLine("\t exit");
                 }
-            }
-            for (int i = 0; i < frequencyKeys.Size; ++i) {
-                if (expectedKeys.IndexOf(frequencyKeys[i]) < 0) {
-                    Error("Unexpected key " + frequencyKeys[i] + " in frequency table");
-                    return;
+                if (command == "list") {
+                    SLinkedList<string> keys = data.Keys;
+                    for (int i = 0; i < keys.Size; i++) {
+                        string key = keys[i];
+                        int value = data[key];
+                        Console.WriteLine(value + " " + key);
+                    }
                 }
-            }
-
-            for (int i = 0; i < frequencyKeys.Size; ++i) {
-                if (frequencyTable[frequencyKeys[i]] != expectedFrequency[expectedKeys[i]]) {
-                    Error("Expecting \"" + frequencyKeys[i] + "\" to equal " + expectedFrequency[expectedKeys[i]] + ", not " + frequencyTable[frequencyKeys[i]]);
+                else if (command == "Exit") {
+                    break;
                 }
-            }
-
-            Huffman.Node huffmanTree = Huffman.MakeHuffmanTree(frequencyTable);
-            PrintTree(huffmanTree);
-            SLinkedList<Huffman.Node> allNodes = new SLinkedList<Huffman.Node>();
-            SLinkedList<Huffman.Node> queue = new SLinkedList<Huffman.Node>();
-            queue.AddTail(huffmanTree);
-            while (queue.Size > 0) {
-                allNodes.AddTail(queue[0]);
-
-                if (queue[0].Left != null) {
-                    queue.AddTail(queue[0].Left);
+                else if (command == "clear") {
+                    Console.Clear();
+                    Console.WriteLine("Enter a command, or help for help");
                 }
-
-                if (queue[0].Right != null) {
-                    queue.AddTail(queue[0].Right);
+                else if (words[0] == "saveBin") {
+                    FileStream stream = new FileStream(words[1], FileMode.CreateNew);
+                    BinaryWriter writer = new BinaryWriter(stream);
+                    Console.WriteLine("Writing data now...");
+                    SLinkedList<string> keys = data.Keys;
+                    writer.Write((byte)data.Size);
+                    for (int i = 0; i < keys.Size; i++) {
+                        string key = keys[i];
+                        int value = data[key];
+                        writer.Write((byte)key.Length);
+                        foreach (char c in key) {
+                            writer.Write((byte)c);
+                        }
+                        //always 4bytes
+                        writer.Write((int)value);
+                    }
+                    Console.WriteLine("Filed Saved");
+                    writer.Dispose();
+                    stream.Dispose();
                 }
-
-                queue.RemoveAt(0);
-            }
-
-
-            SLinkedList<char> expectedNodeData = new SLinkedList<char>();
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('i');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('s');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail(' ');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('\0');
-            expectedNodeData.AddTail('e');
-            expectedNodeData.AddTail('n');
-            expectedNodeData.AddTail('p');
-            expectedNodeData.AddTail('r');
-            expectedNodeData.AddTail('a');
-            expectedNodeData.AddTail('d');
-            expectedNodeData.AddTail('g');
-            expectedNodeData.AddTail('h');
-            expectedNodeData.AddTail('l');
-            expectedNodeData.AddTail('m');
-            expectedNodeData.AddTail('o');
-            expectedNodeData.AddTail('t');
-            expectedNodeData.AddTail('v');
-            expectedNodeData.AddTail('w');
-
-            SLinkedList<int> expectedNodeFrequency = new SLinkedList<int>();
-            expectedNodeFrequency.AddTail(34);
-            expectedNodeFrequency.AddTail(19);
-            expectedNodeFrequency.AddTail(15);
-            expectedNodeFrequency.AddTail(11);
-            expectedNodeFrequency.AddTail(8);
-            expectedNodeFrequency.AddTail(8);
-            expectedNodeFrequency.AddTail(7);
-            expectedNodeFrequency.AddTail(6);
-            expectedNodeFrequency.AddTail(5);
-            expectedNodeFrequency.AddTail(4);
-            expectedNodeFrequency.AddTail(4);
-            expectedNodeFrequency.AddTail(4);
-            expectedNodeFrequency.AddTail(4);
-            expectedNodeFrequency.AddTail(4);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(2);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-            expectedNodeFrequency.AddTail(1);
-
-            bool isBroken = false;
-            for (int i = 0; i < allNodes.Size; ++i) {
-                if (allNodes[i].Data != expectedNodeData[i]) {
-                    Error("Error somewhere in huffman tree");
-                    isBroken = true;
+                else if (words[0] == "loadBin") {
+                    if (File.Exists(words[1])) {
+                        Console.WriteLine("Loading Data...");
+                        using (BinaryReader reader = new BinaryReader(File.Open(words[1], FileMode.Open))) {
+                            int size = reader.ReadByte();
+                            for (int i = 0; i < size; i++) {
+                                int length = (int)reader.ReadByte();
+                                string key = "";
+                                for (int j = 0; j < length; j++) {
+                                    key += reader.ReadChar();
+                                }
+                                int value = reader.ReadInt32();
+                                data.Add(key, value);
+                            }
+                        }
+                        Console.WriteLine("Data has been loaded.");
+                    }
+                    else {
+                        Console.WriteLine("File not found");
+                    }
                 }
-                if (allNodes[i].Frequency != expectedNodeFrequency[i]) {
-                    Error("Error somewhere in huffman tree");
-                    isBroken = true;
-                }
-            }
-            if (isBroken) {
-                return;
-            }
-
-            HashTable<char, BitStream> encodingTable = Huffman.MakeEncodingTable(huffmanTree, new BitStream());
-            SLinkedList<char> keys = encodingTable.Keys;
-            Console.WriteLine("Encoding table:");
-            for (int i = 0; i < keys.Size; ++i) {
-                Console.WriteLine("\t" + keys[i] + ":" + encodingTable[keys[i]].ToString());
-            }
-
-            BitStream tempStream = new BitStream();
-            HashTable<char, BitStream> expectedEncoding = new HashTable<char, BitStream>(char.MaxValue, Huffman.HashChar);
-
-            tempStream = new BitStream();
-            tempStream.Append("01111");
-            expectedEncoding.Add('w', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01110");
-            expectedEncoding.Add('v', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01101");
-            expectedEncoding.Add('t', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("001");
-            expectedEncoding.Add('s', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("1011");
-            expectedEncoding.Add('r', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("1010");
-            expectedEncoding.Add('p', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01100");
-            expectedEncoding.Add('o', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("1001");
-            expectedEncoding.Add('n', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01011");
-            expectedEncoding.Add('m', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01010");
-            expectedEncoding.Add('l', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("11");
-            expectedEncoding.Add('i', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01001");
-            expectedEncoding.Add('h', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("01000");
-            expectedEncoding.Add('g', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("1000");
-            expectedEncoding.Add('e', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("00011");
-            expectedEncoding.Add('d', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("00010");
-            expectedEncoding.Add('a', tempStream);
-            tempStream = new BitStream();
-            tempStream.Append("0000");
-            expectedEncoding.Add(' ', tempStream);
-
-
-            SLinkedList<char> eeKeys = expectedEncoding.Keys;
-            SLinkedList<char> etKeys = encodingTable.Keys;
-
-            for (int i = 0; i < eeKeys.Size; ++i) {
-                if (etKeys.IndexOf(eeKeys[i]) < 0) {
-                    Error("Key " + eeKeys[i] + " not found in encoding table");
-                    return;
-                }
-            }
-
-            for (int i = 0; i < etKeys.Size; ++i) {
-                if (eeKeys.IndexOf(etKeys[i]) < 0) {
-                    Error("Unexpected key: " + etKeys[i] + " found in encoding table");
-                    return;
-                }
-            }
-
-            for (int i = 0; i < etKeys.Size; ++i) {
-                string expectedStr = expectedEncoding[eeKeys[i]].ToString();
-                string actualStr = encodingTable[etKeys[i]].ToString();
-
-                if (expectedStr != actualStr) {
-                    ConsoleColor old = Console.ForegroundColor;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Error, looking at key: " + eeKeys[i] + " (should be the same as: " + etKeys[i] + ")");
-                    Console.WriteLine("\tExpected valaue: " + expectedStr);
-                    Console.WriteLine("\tResulting valaue:" + actualStr);
-
-                    Console.ForegroundColor = old;
-                }
-            }
-
-            Console.ReadLine();
+            } while (true);
         }
 
-        static void PrintTree(Huffman.Node node, int indent = 0) {
-            if (node == null) {
-                return;
+        public static int CustomHash(string arg) {
+            int result = 1;
+            foreach (char c in arg) {
+                result += System.Convert.ToInt32(c);
             }
-            for (int i = 0; i < indent; ++i) {
-                Console.Write('\t');
-            }
-
-            if (!node.IsLeaf) {
-                ConsoleColor old = Console.ForegroundColor;
-                Console.ForegroundColor = ConsoleColor.DarkGray;
-                Console.Write("*");
-                Console.ForegroundColor = old;
-            }
-            else {
-                Console.Write(node.Data);
-            }
-
-            Console.WriteLine(":" + node.Frequency);
-
-            PrintTree(node.Left, indent + 1);
-            PrintTree(node.Right, indent + 1);
+            return result % 100; // hash table is of size 100
         }
     }
 }
