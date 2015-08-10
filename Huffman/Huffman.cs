ff0ab7628c3417520fe4f0a49090b9828f1fb01a
Martin <mszauer@mail.usf.edu>
2015-08-10 12:23:21 -0700

    used contains for huffman frequency table

--- Huffman/Huffman.cs
+++ Huffman/Huffman.cs
@@ -89,18 +89,12 @@ namespace DataStructures {
 
         public static HashTable<char, int> MakeFrequencyTable(string input) {
             HashTable<char, int> result = new HashTable<char, int>(char.MaxValue, HashChar);
-            //loop through the input string
-            foreach (char c in input) {
-                //check if key exists (get the keys of the result hash table)
-                SLinkedList<char> chars = result.Keys;
-                int index = chars.IndexOf(c);
-                //check if key is in hashtable / SLL
-                if (index < 0){ //checks if it contains the char
-                    //add current char with count of 1
-                    result.Add(c,1);
+            //check if key exists (get the keys of the result hash table)
+            foreach (char c in input){
+                if (result.Contains(c)) {
+                    result.Add(c, 1);
                 }
-                else{
-                    //increment count of the character
+                else {
                     result[c] += 1;
                 }
             }
